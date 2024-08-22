4c1c7287b354bd7b180000aa8a3fa45ff70101bb
Dannelyz <114188351+Dannelyz@users.noreply.github.com>
2024-08-22 12:19:48 -0400

    azumy fantasia
    cambio en cart

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -2,26 +2,27 @@
 	idarticulo int primary key identity(1,1),
 	articulo nvarchar(100),
 	estatus nvarchar(20),
-	fechacreate datetime
+	fechacreate datetime,
+	codarticulo VARCHAR(50)
 	)
 
-	ALTER TABLE articulos ADD codarticulo VARCHAR(50);
+
 
 
 	create table sub_articulo (
-	idsubartic int references articulos (idarticulo),
+	idsubartic int ,
 	descripcion nvarchar(200),
 	estatus nvarchar(20),
 	fechacreate datetime,
 	comentario nvarchar(max),
-	urlimagen nvarchar(max)
+	urlimagen nvarchar(max),
+	codarticulo VARCHAR(50),
+	ind_principal VARCHAR(1),
+	grupo_principal int,
+	precio numeric(10,2)
 	)
 
 
-	ALTER TABLE sub_articulo ADD codarticulo VARCHAR(50);
-	ALTER TABLE sub_articulo ADD ind_principal VARCHAR(1);
-	ALTER TABLE sub_articulo ADD grupo_principal int;
-	ALTER TABLE sub_articulo ADD precio numeric(10,2);
 
 
 	create table usuarios (
